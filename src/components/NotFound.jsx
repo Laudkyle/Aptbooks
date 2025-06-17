9334c8da8be12a1772644cf8b36134f1639c74ac
Laudkyle <kyleaby1@gmail.com>
2025-06-17 02:01:08 +0000

    few changes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -75,15 +75,7 @@ const PageNotFound = ({ isMaintenance = false }) => {
           )}
         </div>
       </main>
-
-      {/* Footer */}
-      <footer className="bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-          <p className="text-center text-sm text-gray-500">
-            &copy; {new Date().getFullYear()} AptBooks. All rights reserved.
-          </p>
-        </div>
-      </footer>
+      
     </div>
   );
 };
